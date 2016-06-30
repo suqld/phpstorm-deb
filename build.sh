e5850c5c1a803d4b71118b9184f4b1caa7c8f812
Tim White <timw@suqld.org.au>
2016-06-30 15:15:46 +1000

    Try to upload to bintray

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
-wget https://download.jetbrains.com/webide/PhpStorm-2016.1.2.tar.gz
+VERSION='2016.1.2'
+wget https://download.jetbrains.com/webide/PhpStorm-${VERSION}.tar.gz
 echo 'a397faac301b6c87536873a819677328c7c5684234c6e2d266fc77fd5584853c *PhpStorm-2016.1.2.tar.gz'| sha256sum -c -
 ./update.sh
 debuild -us -uc -b
+curl -T ../phpstorm_${VERSION}_all.deb -utimwsuqld:$BINTRAY_API_KEY https://api.bintray.com/content/timwsuqld/deb/phpstorm/${VERSION}/;deb_distribution=trusty;deb_component=main;deb_architecture=all
