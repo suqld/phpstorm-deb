2a7e0d79d7a7872638da1ef774934b600681f9ff
Tim White <timw@suqld.org.au>
2016-07-01 08:07:03 +1000

    Upload it to the stable distro

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ wget https://download.jetbrains.com/webide/PhpStorm-${VERSION}.tar.gz
 echo 'a397faac301b6c87536873a819677328c7c5684234c6e2d266fc77fd5584853c *PhpStorm-2016.1.2.tar.gz'| sha256sum -c -
 ./update.sh
 debuild -us -uc -b
-curl -T ../phpstorm_${VERSION}_all.deb -utimwsuqld:$BINTRAY_API_KEY "https://api.bintray.com/content/timwsuqld/deb/phpstorm/${VERSION}/pool/main/p/phpstorm_${VERSION}_all.deb;deb_distribution=trusty;deb_component=main;deb_architecture=all"
+curl -T ../phpstorm_${VERSION}_all.deb -utimwsuqld:$BINTRAY_API_KEY "https://api.bintray.com/content/timwsuqld/deb/phpstorm/${VERSION}/pool/main/p/phpstorm_${VERSION}_all.deb;deb_distribution=stable;deb_component=main;deb_architecture=all"
