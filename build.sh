294245075ac163b869cf250b321fa956a1090feb
Tim White <timw@suqld.org.au>
2016-06-30 14:45:29 +1000

    We also need wget

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-wget http://download.jetbrains.com/webide/PhpStorm-2016.1.2.tar.gz
+wget https://download.jetbrains.com/webide/PhpStorm-2016.1.2.tar.gz
 echo 'a397faac301b6c87536873a819677328c7c5684234c6e2d266fc77fd5584853c *PhpStorm-2016.1.2.tar.gz'| sha256sum -c -
 ./update.sh
 debuild -us -uc -b
