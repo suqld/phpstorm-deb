6356cd6ab4a290ecd18eda042b7b85bcf0d6b67d
Tim White <timw@suqld.org.au>
2016-07-14 09:10:02 +1000

    Update for new version

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-VERSION='2016.1.2'
+VERSION='2016.2'
 wget https://download.jetbrains.com/webide/PhpStorm-${VERSION}.tar.gz
-echo 'a397faac301b6c87536873a819677328c7c5684234c6e2d266fc77fd5584853c *PhpStorm-2016.1.2.tar.gz'| sha256sum -c -
+echo "2cba44d6106b82e9acac222683bbd2528a31b3311948aad9fdfff608c321eaa2 *PhpStorm-${VERSION}.tar.gz"| sha256sum -c -
 ./update.sh
 debuild -us -uc -b
 curl -T ../phpstorm_${VERSION}_all.deb -utimwsuqld:$BINTRAY_API_KEY "https://api.bintray.com/content/timwsuqld/deb/phpstorm/${VERSION}/pool/main/p/phpstorm_${VERSION}_all.deb;deb_distribution=stable;deb_component=main;deb_architecture=all"
